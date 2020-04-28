b6364e5b5b3c3e53763328cbbf16aab0d863e866
WalkWind996 <1035314372@qq.com>
2020-04-28 18:36:10 +0800

    Initial commit

---
+++ src/main/java/com/walkwind/actuator/BootSenior07ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.walkwind.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSenior07ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSenior07ActuatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/walkwind/actuator/BootSenior07ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walkwind.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSenior07ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
